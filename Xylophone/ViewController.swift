cb7e695f455c273be40723c69e79aab3d70d2aeb
wanching0730 <wanching_97@hotmail.com>
2018-01-03 23:10:55 +0800

    Initial Commit

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Wan Ching on 03/01/2018.
+//  Copyright © 2018 Wan Ching. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ XylophoneTests/XylophoneTests.swift
@@ -0,0 +1,36 @@
+//
+//  XylophoneTests.swift
+//  XylophoneTests
+//
+//  Created by Wan Ching on 03/01/2018.
+//  Copyright © 2018 Wan Ching. All rights reserved.
+//
+
+import XCTest
+@testable import Xylophone
+
+class XylophoneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
